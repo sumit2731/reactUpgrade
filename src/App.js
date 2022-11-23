c6650f3b5b8d1cd521b47456f700b9383ad4657a
sumit2731 <sumitsood3127@gmail.com>
2022-11-23 13:32:52 +0530

    Added Project files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Cmp from './Cmp1';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Cmp showText ={false}></Cmp>
+    </div>
+  );
+}
+
+export default App;
